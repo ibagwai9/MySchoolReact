1940d6a108b36e3d322b0110055b03a7f3af7524
ibagwai9 <ibagwai9@gmail.com>
2021-10-09 06:19:04 +0100

    x

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,5 @@
 <?php
-
+use Illuminate\Foundation\Application;
 /*
 |--------------------------------------------------------------------------
 | Create The Application
@@ -11,7 +11,7 @@
 |
 */
 
-$app = new Illuminate\Foundation\Application(
+$app = new Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
